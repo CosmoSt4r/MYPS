320700338c99bae785881aae09e8707584cf724c
Andrey Kalkutin <cosmostar@protonmail.com>
2020-09-03 17:32:54 +0500

    'Sign Up' page added

--- main.py
+++ main.py
@@ -45,6 +45,11 @@ def login():
             return render_template('login.html')
 
 
+@app.route("/signup/", methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run()
